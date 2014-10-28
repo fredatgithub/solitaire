3de3e5b4c5e2513addd59df0cb8c4323b03acae4
Freddy Juhel <domaine75@gmail.com>
2014-10-28 17:08:10 +0100

    adding properties to class columnOfCards

--- Solitaire/ColumnOfCards.cs
+++ Solitaire/ColumnOfCards.cs
@@ -7,7 +7,8 @@ namespace Solitaire
     public byte ColumnNumber { get; set; }
     public byte NumberOfCardsInColumn { get; set; }
     public bool IsEmpty { get; set; }
-    // add tail and head cards
+    public Card TailCard { get; set; }
+    public Card HeadCard { get; set; }
     public ArrayList CardList { get; set; }
 
     public ColumnOfCards()
